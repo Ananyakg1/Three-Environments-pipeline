440e2e129ea18efe7b1785b5206dc5d4df18e649
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 16:05:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,39 +1,17 @@
-variable "location" {
-  type    = string
-  default = "East US"
-}
-
-variable "environment" {
-  type    = string
-}
-
 variable "subscription_id" {
-  type    = string
+  type = string
 }
 
 variable "tenant_id" {
-  type    = string
+  type = string
 }
 
 variable "client_id" {
-  type    = string
+  type = string
 }
 
 variable "client_secret" {
-  type    = string
-}
-
-variable "resource_group_name" {
   type = string
 }
-
-variable "storage_account_name" {
-  type = string
-}
-
-variable "container_name" {
-  type = string
-}
-
 
 
