c50def19222572f67f75973887117a6dc0b70238
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 15:56:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 terraform {
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name   = "new1-resource-group"
+    storage_account_name  = "mystorageaccount"
+    container_name        = "new-container"
+    key                   = "terraform.tfstate"
+  }
 
   required_providers {
     azurerm = {
@@ -11,29 +16,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
 }
 
