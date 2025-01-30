2ff19e1e5ae623e1e04d7dde4b6ec378fe760639
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 15:56:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,39 +1,15 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "new-resource-group"
-    storage_account_name  = "new-storageaccount"
-    container_name        = "new-container"
-    key                   = "terraform.tfstate"
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
-
-  # Default provider for general use
-}
-
-# Dev environment provider
-provider "azurerm" {
-  alias           = "dev"
+  alias    = "dev"
   features {}
 }
 
-# QA environment provider
 provider "azurerm" {
-  alias           = "qa"
+  alias    = "qa"
   features {}
 }
 
-# Prod environment provider
 provider "azurerm" {
-  alias           = "prod"
+  alias    = "prod"
   features {}
 }
+
