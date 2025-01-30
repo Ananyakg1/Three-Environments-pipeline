ebe52aa3938a8fb94ff512717bf72f14dace950d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 12:32:39 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+# outputs.tf
 output "resource_group_name" {
   value = azurerm_resource_group.example.name
 }
@@ -9,3 +10,4 @@ output "storage_account_name" {
 output "storage_container_name" {
   value = azurerm_storage_container.example.name
 }
+
