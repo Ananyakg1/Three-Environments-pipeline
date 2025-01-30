bdfeaccb2af8f4b09eaf33d894375bb3051c6f94
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 12:33:11 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
+# providers.tf
 provider "azurerm" {
-  features = {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  features {}
+
+  # For using different environments, you may want to configure different aliases
+  alias = "dev"  # You can create different alias configurations if needed
 }
