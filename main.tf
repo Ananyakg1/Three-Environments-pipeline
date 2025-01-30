15deb5c56785a59cd0cca451513f5522e71dc454
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 12:27:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,25 @@
+# main.tf
 provider "azurerm" {
-  features = {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  features {}
 }
 
-# Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "${var.environment}-resource-group"
+  name     = "rg-${var.environment}"
   location = var.location
 }
 
-# Storage Account
 resource "azurerm_storage_account" "example" {
-  name                     = "${var.environment}storageacc"
+  name                     = "st${var.environment}account"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier              = "Standard"
   account_replication_type = "LRS"
 }
 
-# Storage Container
 resource "azurerm_storage_container" "example" {
-  name                  = "${var.environment}-container"
+  name                  = "container-${var.environment}"
   storage_account_name = azurerm_storage_account.example.name
-  container_access_type = "private"
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
-}
+# Removed outputs defined earlier in the code
 
-output "storage_account_name" {
-  value = azurerm_storage_account.example.name
-}
-
-output "storage_container_name" {
-  value = azurerm_storage_container.example.name
-}
