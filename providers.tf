075f569c107ab29d59ddb33ed8fdde87a77ee21d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 15:42:17 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,39 @@
-# providers.tf
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "new-resource-group"
+    storage_account_name  = "new-storageaccount"
+    container_name        = "new-container"
+    key                   = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 
-  # For using different environments, you may want to configure different aliases
-  alias = "dev"  # You can create different alias configurations if needed
+  # Default provider for general use
+}
+
+# Dev environment provider
+provider "azurerm" {
+  alias           = "dev"
+  features {}
+}
+
+# QA environment provider
+provider "azurerm" {
+  alias           = "qa"
+  features {}
+}
+
+# Prod environment provider
+provider "azurerm" {
+  alias           = "prod"
+  features {}
 }
