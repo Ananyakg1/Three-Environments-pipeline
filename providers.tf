4fd4ea99129b029d65d5a1770fdd8508c6688f60
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 16:03:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,9 @@
 provider "azurerm" {
-  alias    = "dev"
   features {}
-}
 
-provider "azurerm" {
-  alias    = "qa"
-  features {}
-}
-
-provider "azurerm" {
-  alias    = "prod"
-  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
