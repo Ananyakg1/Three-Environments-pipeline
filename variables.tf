70128c059b48ea250b3e80e2058f68d85e2c9c1a
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-31 14:45:58 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,37 +1,12 @@
-# Global Variables for Terraform
-
 variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "East US"
-}
-
-variable "resource_group_name" {
-  description = "Azure Resource Group name"
+  description = "The location for the resource group"
   type        = string
 }
 
-variable "storage_account_name" {
-  description = "Azure Storage Account for Terraform state"
+variable "env" {
+  description = "The environment name"
   type        = string
 }
 
-variable "container_name" {
-  description = "Azure Blob container for Terraform state"
-  type        = string
-}
-
-variable "environment" {
-  description = "Deployment environment (dev, qa, prod)"
-  type        = string
-}
-
-variable "tags" {
-  description = "Resource tags"
-  type        = map(string)
-  default = {
-    Environment = "dev"
-  }
-}
 
 
