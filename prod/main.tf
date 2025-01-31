38c0df4bfb38d7c40b4002ddd16e614e0c554c6d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-31 14:42:57 +0530

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -1,12 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_resource_group" "example" {
+  name     = "harsharg-${var.env}"
+  location = var.location
 }
