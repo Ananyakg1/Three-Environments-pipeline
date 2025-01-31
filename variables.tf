f583c4dafede967900a347637444fd1049055ed0
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-31 10:50:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,37 @@
-variable "subscription_id" {
-  type = string
+# Global Variables for Terraform
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
 }
 
-variable "tenant_id" {
-  type = string
+variable "resource_group_name" {
+  description = "Azure Resource Group name"
+  type        = string
 }
 
-variable "client_id" {
-  type = string
+variable "storage_account_name" {
+  description = "Azure Storage Account for Terraform state"
+  type        = string
 }
 
-variable "client_secret" {
-  type = string
+variable "container_name" {
+  description = "Azure Blob container for Terraform state"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, qa, prod)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+  default = {
+    Environment = "dev"
+  }
 }
 
 
