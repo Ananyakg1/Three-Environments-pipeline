b85de6a81e05eebfacb0f631e2663bfe0ae1a4ce
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-03 09:38:56 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,9 +1,19 @@
-provider "azurerm" {
-  features {}
+resource "azurerm_resource_group" "rg" {
+  name     = "dem-resource-group"
+  location = var.location
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "harsharg-${var.env}"
-  location = var.location
+resource "azurerm_storage_account" "storage" {
+  name                     = "demstorageacc"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "dem-container"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
 
